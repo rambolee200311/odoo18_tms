7d7a1285111aa1c16af3ddc7e6bf4c02a96f6acd
李建强 <rambolee200311@hotmail.com>
2026-08-10 14:19:27 +0800

    Sprint52-Fix3: add Start Inquiry/Start Quote button bar on request page (1.0.127)

--- addons/wd_tlms/__manifest__.py
+++ addons/wd_tlms/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Transport Logistics Management',
-    'version': '1.0.126',
+    'version': '1.0.127',
     'category': 'Transport',
     'summary': '3PL Transport Management System',
     'description': '''
